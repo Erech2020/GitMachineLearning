b59b84ac98ec216649645c83a6fd8832fd630c9d
Erech2020 <ingerech@unamba.edu.pe>
2019-05-27 18:15:15 -0500

    primer archivo

---
+++ imprimir02.py
@@ -0,0 +1,8 @@
+print('Python')  # muestra: Python  
+luna = 385000  # asigna valor a variable
+print('D.M. Tierra-Luna', luna, 'km')  # D.M. Tierra-Luna 385000 km
+print('D.M. Tierra-Luna ' + str(luna)+' km')  # D.M. Tierra-Luna 385000 km
+#print('\nHola\npythonisos\n')  # muestra cadena en varias líneas
+print('Continuará...', end=' ')  # ejecuta varios print() en misma... 
+print('otro día')  # ...línea. Muestra: Continuará... otro día
+print()  # inserta una línea en blanco
